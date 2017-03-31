5a8818439c98bc38a4617e3f6c8d3a20d71c050e
Caley Brock <caleybrock7@gmail.com>
2017-03-31 16:53:04 -0700

    Use Jasmine spies instead of sinon spies

--- test/js/service/PiskelApiServiceTest.js
+++ test/js/service/PiskelApiServiceTest.js
@@ -1,4 +1,4 @@
-
+//https://jasmine.github.io/1.3/introduction.html
 describe("PiskelApiService test suite", function() {
   var piskelWindow;
 
@@ -10,42 +10,45 @@ describe("PiskelApiService test suite", function() {
     var service;
     beforeEach(function() {
       service = new pskl.service.PiskelApiService();
-      sinon.stub($, "subscribe");
+      spyOn($, "subscribe");
       service.init(piskelWindow);
     });
 
     afterEach(function() {
-      $.subscribe.restore();
+      $.subscribe.calls.reset();
     });
 
     it("sends a PISKEL_API_READY message to the parent window", function() {
-      expect(piskelWindow.parent.postMessage.callCount).toBe(1);
-      expect(piskelWindow.parent.postMessage.getCall(0).args[0].type).toBe(PiskelApi.MessageType.PISKEL_API_READY);
+      expect(piskelWindow.parent.postMessage.calls.count()).toEqual(1);
+      expect(piskelWindow.parent.postMessage.calls.argsFor(0)[0].type).toBe(PiskelApi.MessageType.PISKEL_API_READY);
     });
 
     it("adds an event listener on its own window", function() {
-      expect(piskelWindow.addEventListener.calledOnce).toBe(true);
+      expect(piskelWindow.addEventListener.calls.count()).toEqual(1);
     });
 
     it("subscribes to events", function() {
-      expect($.subscribe.callCount).toBe(5);
-      expect($.subscribe.args[0][0]).toBe(Events.PISKEL_SAVE_STATE);
-      expect($.subscribe.args[1][0]).toBe(Events.FPS_CHANGED);
-      expect($.subscribe.args[2][0]).toBe(Events.HISTORY_STATE_LOADED);
-      expect($.subscribe.args[3][0]).toBe(Events.FRAME_SIZE_CHANGED);
-      expect($.subscribe.args[4][0]).toBe(Events.ADD_NEW_FRAME_CLICKED);
+      expect($.subscribe.calls.count()).toEqual(5);
+      expect($.subscribe.calls.argsFor(0)[0]).toBe(Events.PISKEL_SAVE_STATE);
+      expect($.subscribe.calls.argsFor(1)[0]).toBe(Events.FPS_CHANGED);
+      expect($.subscribe.calls.argsFor(2)[0]).toBe(Events.HISTORY_STATE_LOADED);
+      expect($.subscribe.calls.argsFor(3)[0]).toBe(Events.FRAME_SIZE_CHANGED);
+      expect($.subscribe.calls.argsFor(4)[0]).toBe(Events.ADD_NEW_FRAME_CLICKED);
     });
   });
 
   function createFakePiskelWindow() {
-    return {
-        parent: {
-          postMessage: sinon.spy(),
-        },
-        location: {
-          origin: ''
-        },
-        addEventListener: sinon.spy()
-      };
+    var piskelWindow = {
+      parent: {
+        postMessage: function() {},
+      },
+      location: {
+        origin: ''
+      },
+      addEventListener: function() {}
+    };
+    spyOn(piskelWindow.parent, 'postMessage');
+    spyOn(piskelWindow, 'addEventListener');
+    return piskelWindow;
   }
 });
