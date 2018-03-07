030221018501c1437563d5fdae87f9951d4f1fc7
Erin <erin@code.org>
2018-03-06 17:54:27 -0800

    Expand test to include image with more than one frame

--- test/js/utils/CanvasUtilsTest.js
+++ test/js/utils/CanvasUtilsTest.js
@@ -1,15 +1,28 @@
 describe("Canvas utils", function() {
 
-  it("returns a single blank frame when provided a blank spritesheet set to ignore blank frames", function() {
-    var width = 10;
-    var height = 10;
-    var blankData = pskl.utils.CanvasUtils.createCanvas(width, height).toDataURL();
-    var blankImg = new Image(width, height);
+  it("returns a single blank frame when provided a small blank spritesheet set to ignore blank frames", function() {
+    var imageSize = 10;
+    var blankData = pskl.utils.CanvasUtils.createCanvas(imageSize, imageSize).toDataURL();
+    var blankImg = new Image(imageSize, imageSize);
     blankImg.src = blankData;
 
-    var blankSpritesheetFrames = pskl.utils.CanvasUtils.createFramesFromImage(blankImg, 0, 0, width, height, true, true);
+    var blankSpritesheetFrames = pskl.utils.CanvasUtils.createFramesFromImage(blankImg, 0, 0, imageSize, imageSize, true, true);
 
     expect(blankSpritesheetFrames.length).toBe(1);
     expect(blankSpritesheetFrames[0].toDataURL()).toBe(blankData);
+  });
+
+  it("returns a single blank frame when provided a large blank spritesheet set to ignore blank frames", function() {
+    var imageSize = 50;
+    var frameSize = 10;
+    var blankData = pskl.utils.CanvasUtils.createCanvas(imageSize, imageSize).toDataURL();
+    var blankImg = new Image(imageSize, imageSize);
+    blankImg.src = blankData;
+
+    var blankSpritesheetFrames = pskl.utils.CanvasUtils.createFramesFromImage(blankImg, 0, 0, frameSize, frameSize, true, true);
+
+    expect(blankSpritesheetFrames.length).toBe(1);
+    expect(blankSpritesheetFrames[0].width).toBe(frameSize);
+    expect(blankSpritesheetFrames[0].height).toBe(frameSize);
   });
 });
