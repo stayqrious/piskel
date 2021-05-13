a6a3509844c926834b2962f6c01a864e60875c30
Salvador Villalon <salvadorvillalon54@gmail.com>
2021-05-13 15:52:33 -0700

    saving changes

--- src/js/controller/settings/exportimage/ExportController.js
+++ src/js/controller/settings/exportimage/ExportController.js
@@ -92,4 +92,80 @@
   ns.ExportController.prototype.getExportZoom = function () {
     return parseInt(this.widthInput.value, 10) / this.piskelController.getWidth();
   };
+
+  ns.ExportController.prototype.populateResizeSectionTemplate = function (templateDataArray) {
+    var obj = { 'text': '', 'inputText': '' };
+    var resultHtml = '';
+
+    for (var i = 0; i < templateDataArray.length; i++) {
+      var templateToUse = templateDataArray[i];
+      var templateId = templateToUse[0];
+      var text = templateToUse[1];
+      var inputText = templateToUse[2];
+
+      obj.text = text;
+      obj.inputText = inputText;
+
+      var templateWithValues = pskl.utils.Template.fillInTemplate(templateId, obj);
+      resultHtml += templateWithValues;
+
+      obj = { 'text': '', 'inputText': '' };
+    }
+
+    return resultHtml;
+  };
+
+  ns.ExportController.prototype.createExportTitle = function (i18n) {
+    var templateDataArray = [
+      ['export-settings-template', i18n.resizeTitle()]
+    ];
+    return this.populateResizeSectionTemplate(templateDataArray);
+  };
+
+  ns.ExportController.prototype.createResizeCanvasForm = function (i18n) {
+    var templateDataArray = [
+      ['resize-tool-template', i18n.widthTitle(), 'resize-width'],
+      ['resize-tool-template', i18n.heightTitle(), 'resize-height'],
+      ['ratio-canvas-template', i18n.maintainAspectRatio(), 'resize-content-checkbox'],
+      ['ratio-canvas-template', i18n.resizeCanvasContent(), 'resize-ratio-checkbox'],
+      ['resize-anchor-template', i18n.anchor()],
+      ['resize-button-template', i18n.resizeSubmitButton()],
+    ];
+    return this.populateResizeSectionTemplate(templateDataArray);
+  };
+
+  ns.ExportController.prototype.createResizeCanvasDefaultForm = function (i18n) {
+    var templateDataArray = [
+      ['resize-tool-template', i18n.widthTitle(), 'default-width'],
+      ['resize-tool-template', i18n.heightTitle(), 'default-height'],
+      ['resize-button-template', i18n.resizeSubmitButtonDefault()],
+    ];
+
+    return this.populateResizeSectionTemplate(templateDataArray);
+  };
+
+  /**
+  * @private
+  */
+  ns.ExportController.prototype.createExportDom_ = function (i18n) {
+    var html = '';
+    var exportTitleHtml = this.createExportTitle(i18n);
+    html += exportTitleHtml;
+    $('#settings-title').html(html);
+
+    html = '';
+    var resizeCanvasFormHtml = this.createResizeCanvasForm(i18n);
+    html += resizeCanvasFormHtml;
+    $('#resize-canvas-form').html(html);
+
+    html = '';
+    var resizeTitleDefaultHtml = this.createResizeTitleDefault(i18n);
+    html += resizeTitleDefaultHtml;
+    $('#settings-title-default').html(html);
+
+    html = '';
+    var resizeCanvasFormDefaultHtml = this.createResizeCanvasDefaultForm(i18n);
+    html += resizeCanvasFormDefaultHtml;
+    $('#default-size-form').html(html);
+  };
 })();
