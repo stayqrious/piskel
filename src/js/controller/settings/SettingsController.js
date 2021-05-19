f0f50ec8a9f2dba9d28e7c12719b75261e65738b
Salvador Villalon <salvadorvillalon54@gmail.com>
2021-05-19 16:22:23 -0700

    saving changes

--- src/js/controller/settings/SettingsController.js
+++ src/js/controller/settings/SettingsController.js
@@ -27,11 +27,12 @@
       controller : ns.SaveController
     }
   };
+  var i18n;
+
   var SEL_SETTING_CLS = 'has-expanded-drawer';
   var EXP_DRAWER_CLS = 'expanded';
 
-  ns.SettingsController = function (piskelController, i18n) {
-    this.i18n = i18n;
+  ns.SettingsController = function (piskelController) {
     this.piskelController = piskelController;
     this.closeDrawerShortcut = pskl.service.keyboard.Shortcuts.MISC.CLOSE_POPUP;
     this.settingsContainer = document.querySelector('[data-pskl-controller=settings]');
@@ -43,7 +44,10 @@
   /**
    * @public
    */
-  ns.SettingsController.prototype.init = function () {
+  ns.SettingsController.prototype.init = function (i18nPassed) {
+    i18n = i18nPassed;
+    this.createSettingsDom_();
+
     pskl.utils.Event.addEventListener(this.settingsContainer, 'click', this.onSettingsContainerClick_, this);
     pskl.utils.Event.addEventListener(document.body, 'click', this.onBodyClick_, this);
 
@@ -85,8 +89,8 @@
     this.destroyCurrentController_();
 
     this.currentSetting = setting;
-    this.currentController = new settings[setting].controller(this.piskelController, this.i18n);
-    this.currentController.init();
+    this.currentController = new settings[setting].controller(this.piskelController);
+    this.currentController.init(i18n);
 
     pskl.app.shortcutService.registerShortcut(this.closeDrawerShortcut, this.closeDrawer_.bind(this));
 
@@ -119,5 +123,48 @@
         this.currentController = null;
       }
     }
+  };
+
+   /**
+   * @private
+   */
+  ns.SettingsController.prototype.createSettingsDom_ = function () {
+    console.log('Inside createSettingsDom_()');
+    var html = '';
+    var tpl = pskl.utils.Template.get('settings-template');
+    var result1 = pskl.utils.Template.replace(tpl, {
+      dataSetting: 'user',
+      iconSetting: 'icon-settings-gear-white',
+      title: '<span class=\'highlight\'>PREFERENCES</span></br>'
+    });
+
+    var changeTitle = '<span class=\'highlight\'>' + i18n.resizeSetting() + '</span></br>' + i18n.resizeSettingFirst();
+    var tpl = pskl.utils.Template.get('settings-template');
+    var result2 = pskl.utils.Template.replace(tpl, {
+      dataSetting: 'resize',
+      iconSetting: 'icon-settings-resize-white',
+      title: changeTitle
+    });
+
+    changeTitle = '<span class=\'highlight\'>' + i18n.saveSetting() + '</span></br>' + i18n.saveSettingFirst() + '<br/>' + i18n.saveSettingSecond();
+    var tpl = pskl.utils.Template.get('settings-template');
+    var result3 = pskl.utils.Template.replace(tpl, {
+      dataSetting: 'save',
+      iconSetting: 'icon-settings-save-white',
+      title: changeTitle
+    });
+
+    changeTitle = '<span class=\'highlight\'>' + i18n.exportSetting() + '</span></br>' + i18n.exportSettingFirst() + '<br/>' + i18n.exportSettingSecond();
+    var tpl = pskl.utils.Template.get('settings-template');
+    var result4 = pskl.utils.Template.replace(tpl, {
+      dataSetting: 'export',
+      iconSetting: 'icon-settings-export-white',
+      title: changeTitle
+    });
+
+    html = html + result1 + result2 + result3 + result4;
+
+    console.log(html);
+    $('#verticalCenterer').html(html);
   };
 })();
