fdae4207f18f32abf877b9f5a0b2a34b315130c8
Salvador Villalon <salvadorvillalon54@gmail.com>
2021-05-20 13:36:09 -0700

    saving changes

--- src/js/controller/PenSizeController.js
+++ src/js/controller/PenSizeController.js
@@ -1,7 +1,8 @@
 (function () {
   var ns = $.namespace('pskl.controller');
 
-  ns.PenSizeController = function () {
+  ns.PenSizeController = function (i18n) {
+    this.createPenSizeDom_(i18n);
     this.sizePicker = new pskl.widgets.SizePicker(this.onSizePickerChanged_.bind(this));
   };
 
@@ -23,5 +24,22 @@
   ns.PenSizeController.prototype.updateSelectedOption_ = function () {
     var size = pskl.app.penSizeService.getPenSize();
     this.sizePicker.setSize(size);
+  };
+
+  ns.PenSizeController.prototype.createChangePenSize = function (i18n) {
+    var templateValues = {
+      title: i18n.penSizeChangeTitle() + '</br>' + i18n.penSizeChangeAt14816(),
+    };
+    var templateId = 'change-pen-size-template';
+    return pskl.utils.Template.fillInTemplate(templateId, templateValues);
+  };
+
+  /**
+   * @private
+   */
+  ns.PenSizeController.prototype.createPenSizeDom_ = function (i18n) {
+    var html = '';
+    html += this.createChangePenSize(i18n);
+    $('#pen-size-container').html(html);
   };
 })();
